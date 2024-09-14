9efbe3768aa36aff33212f4bb6bcd524b8521280
Isaac Adeleke <158352924+IsaacAdeleke-coding@users.noreply.github.com>
2024-09-14 10:43:16 -0700

    Update main.cpp
    Had issues commiting from Xcode.

--- class-activity-03/main.cpp
+++ class-activity-03/main.cpp
@@ -6,9 +6,26 @@
 //
 
 #include <iostream>
+#include <cstdlib>  // For rand() and srand()
+#include <ctime>    // For time()
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main() {
+    // Seed the random number generator with the current time
+    // Using bitwise AND with 0xFFFFFFFF to ensure it fits within an unsigned int
+    srand(time(0) & 0xFFFFFFFF);
+
+    int minValue = 1;
+    int maxValue = 6;
+
+    // Rolling two dice using the formula [rand() % (maxValue - minValue + 1)] + minValue
+    int die1 = (rand() % (maxValue - minValue + 1)) + minValue;
+    int die2 = (rand() % (maxValue - minValue + 1)) + minValue;
+
+    // Output the result
+    cout << "Die 1: " << die1 << ", Die 2: " << die2 << endl;
+
     return 0;
 }
+
